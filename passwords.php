34da342264c39891475051983582ab1014a58915
Andrew <atl9gz@virginia.edu>
2015-06-18 10:58:21 -0400

    Two factor authentication text added

--- passwords.php
+++ passwords.php
@@ -9,6 +9,7 @@
 </head>
 <body>
 	<?php include 'header.php';?>
+	<br>
 	<div class="container-fluid">
 		<div class="row" style="text-align:center;">
 			<div class="col-md-8 col-md-offset-2">
@@ -28,7 +29,7 @@
 					<li>Basic transformations (dr4g0n)</li>
 					<li>A brute force search (aaa, aab, aac, ...)</li>
 				</ul>
-				A longer password and inclusion of symbols, numbers, uppercase, and lowercase characters expands the number of potential combinations 
+				A longer password and inclusion of symbols, numbers, uppercase, and lowercase characters increases the number of potential combinations 
 				by expanding the search space. A password containing 12 or more characters is highly reommended because at 350 billion password cracks per second would take
 				<?php
 				echo number_format(((pow(95, 12)/350000000000)/60), 2)." minutes or ";
@@ -38,7 +39,7 @@
 				echo number_format(((pow(95, 12)/350000000000)/60/60/24/365/1000), 2)." centuries ";
 				echo "to break in a best case scenario."
 				?>
-				A password containing padding that includes an uppercase and lowercase character, symbol, and digit like U7^d can make a strong password even stronger.
+				A password containing padding that includes an uppercase and lowercase character, symbol, and digit like U7^d can make a password even stronger.
 				Reusing passwords on sites is not recommended because even though one site might be safe, other sites might not
 				be secure and store their passwords in plaintext or other insecure manner<br>
 			</div>
@@ -53,7 +54,24 @@
 		</div>
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2">
-        </div>
+				<h2>Two Factor Authentication</h2>
+				Having a strong password is one way to avoid having one's account being compromised on the internet. Two factor authentication
+				is another way to greatly increase the security on an account. Two factor authentication contains two of three things
+				<ul>
+				<li>Something you are (fingerprint)</li>
+				<li>Something you have (generated key from an app or a security card)</li>
+				<li>Something you know (password or an answer to a question)</li>
+			</ul>
+				Two factor authentication is usually a password and a generated one time use key.
+				This key can be sent by a text message or accessed by an app on one's phone or other key generating device.
+				If a hacker does obtain access to a password, two factor makes the hacker still most likely unable to access the account, the
+				hacker won't have access to the one time key which is required to log in.<br><br>
+				There are some disadvantages to two factor authentication. If the device is ever lost, then the user cannot access their account
+				and has to go through a process in order to reset the account, though as precaution against this some services offer backup
+				codes that can be printed and stored in a wallet. The device also has to be around at all times with the user.<br><br>
+				Links to two step verification sites. <a href="https://www.google.com/landing/2step/">https://www.google.com/landing/2step/</a>
+			</div>
+		</div>
     </div>
         <!--
 		<div class="row">
@@ -92,25 +110,6 @@
 				</select>
 				<input type='submit' class='btn btn-default' onclick="hash()">
 				<span id='result'></span>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-				<h2>Two Factor Authentication</h2>
-				Having a strong password is a way to avoid having one's account being compromised on the internet. Two factor authentication
-				is another way to greatly increase the probability that a person is who they claim to be on a computer. Two factor authentication
-				contains two of three things.
-				-Something you are (fingerprint)
-				-Something you have (generated key from an app or a security card)
-				-Something you know (password or an answer to a question)<br><br>
-				For the most part, two factor authentication is something you know and have. A password and a generated one time key are usually
-				what is used. This one time key can be sent by a text message or accessed by an app on one's phone or other key generating device.
-				If a hacker does obtain access to a password, two factor makes the hacker still most likely unable to access the account. The
-				hacker won't have access to the one time key which is required to log in.<br><br>
-				There are some disadvantages to two factor authentication. If the device is ever lost, then the user cannot access their account
-				and has to go through a process in order to reset the account, though as precaution against this some services offer backup
-				codes that can be printed and stored in a wallet. The device also has to be around at all times with the user.<br><br>
-				Links to two step verification sites. <a href="https://www.google.com/landing/2step/">https://www.google.com/landing/2step/</a>
 			</div>
 		</div>
 	-->
