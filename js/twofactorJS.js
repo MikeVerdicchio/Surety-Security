6d61644f544ec4664156772037e38b4a726db2e7
Mike Verdicchio <mikeverdicchio@gmail.com>
2015-06-18 13:49:26 -0400

    TwoFactor JS fixes (again)

--- js/twofactorJS.js
+++ js/twofactorJS.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('#submit').click(function () {
-        if (document.getElementById("username").textContent.equals("SuretySecurity") && document.getElementById("password").textContent.equals("helloworld")) {
+        if ( (document.getElementById("username").textContent === "SuretySecurity") && (document.getElementById("password").textContent === "helloworld") ) {
             var code = Math.floor((Math.random() * 999999) + 100000);
             window.alert(code);
             var input = window.prompt("Please enter the code given to you.", "");
@@ -13,7 +13,5 @@ $(document).ready(function () {
         } else {
             window.alert("Incorrect username and/or password. Please try again.");
         }
-
-        return false;
     });
 });
