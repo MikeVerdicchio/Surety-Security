528f051adfdc9004bb56fd43ff9fee150084afbb
Mike Verdicchio <mikeverdicchio@gmail.com>
2015-06-18 14:57:14 -0400

    Finished 2FA example

--- js/twofactorJS.js
+++ js/twofactorJS.js
@@ -1,16 +1,25 @@
-$(document).ready(function () {
-    $('#submit').click(function () {
+$(document).ready(function() {
+    $('#submit').click(function() {
         if ((document.getElementById("username").value === "SuretySecurity") && (document.getElementById("password").value === "helloworld")) {
             var code = Math.floor((Math.random() * 999999) + 100000);
-            var input = prompt("Your generated code is " + code + ". Please enter it below to log in.", code);
+            var input = prompt("The Code Generator has created the following code for you: " + code + ". Please enter it below to log in.", code);
 
-			if (code == input) {
-                window.alert("Access granted!");
+            if (code == input) {
+                $('#response').removeClass("alert alert-info text-center");
+                $('#response').removeClass("alert alert-danger text-center");
+                $('#response').addClass("alert alert-success text-center");
+                $('#response').html("<strong>Success! </strong>You have logged in.");
             } else {
-                window.alert("Incorrect code.");
+                $('#response').removeClass("alert alert-info text-center");
+                $('#response').removeClass("alert alert-success text-center");
+                $('#response').addClass("alert alert-danger text-center");
+                $('#response').html("<strong>Error! </strong>Incorrect authentication code. Please try again.");
             }
         } else {
-            window.alert("Incorrect username and/or password. Please try again.");
+            $('#response').removeClass("alert alert-info text-center");
+            $('#response').removeClass("alert alert-success text-center");
+            $('#response').addClass("alert alert-danger text-center");
+            $('#response').html("<strong>Error! </strong>Incorrect username and/or password. Please try again.");
         }
     });
 });
