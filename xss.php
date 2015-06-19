5ee5ad55454c95379b355df78de3ca29d89ddb7a
Andrew <atl9gz@virginia.edu>
2015-06-19 11:31:17 -0400

    XSS changes

--- xss.php
+++ xss.php
@@ -23,31 +23,45 @@
 		</div>
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2">
-				<?php
-				$param = "";
-				if (isset($_GET['xssparam'])){
-					$param = $_GET['xssparam'];
-				}
-				echo "<input type='hidden' value=".$param.">";
-				?>
+				<h2>Reflected</h2>
+				Malicious code is sent as input to a server and the code is then "reflected" back to the user
+				and displayed (seen or unseen) somewhere on the page.
+			</div>
+		</div>
+				<div class="row">
+			<div class="col-md-8 col-md-offset-2">
 				<h3>Example</h3>
 				This is an example of a XSS attack in which an object on the page is automatically
 				filled in with a parameter in the URL. The parameter is not sanitized before being
 				put into the object which can result in code being passed in.
-				Put this at the end of the url and see what happens
+				<br><code>$param = '';
+				if (isset($_GET&#91;'xssparam'&#93;)){
+				$param = $_GET&#91;'xssparam'&#93;;}
+				echo "&lt;input type='hidden' value=.$param.&gt;";</code><br>
+				The code sets a hidden input's value to the GET paramater value directly without
+				checking if the value is safe and valid.<br>
+				Put either of these code snipets at the end of the url and see what happens!
+				The first example might not work with some browsers, as they might block anything with
+				the script tag.
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2">
 				<pre><?php echo htmlspecialchars("?xssparam=><script type='text/javascript'>document.body.innerHTML = '';</script><p></p");?></pre>
+				This is a very basic Javascript line of code that sets the html document body to blank, but this isn't the only thing a hacker can do.
+				Rearranging the site's layout and adding or removing elements are just a couple of the things possible with Javascript manipulation.
+				Most browsers block any url with the script tag to provide users protection against such URL attacks.
+				<pre><?php echo htmlspecialchars("?xssparam=><b></p");?></pre>
+				This injects a HTML bold tag, which was injected by a GET parameter, for the value in the hidden input. This results in the hidden
+				input having the bold tag for its value and the rest of the page below the input being bold.
 			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-				<h2>Reflected</h2>
-				Malicious code is sent as input to a server and the code is then "reflected" back to the user
-				and displayed (seen or unseen) somewhere on the page.
-			</div>
+			<?php
+			$param = "";
+			if (isset($_GET['xssparam'])){
+				$param = $_GET['xssparam'];
+			}
+			echo "<input type='hidden' value=".$param.">";
+			?>
 		</div>
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2">
@@ -82,9 +96,9 @@
 				}
 				?>
 			-->
-			</div>
 		</div>
 	</div>
-	<?php include 'footer.php';?>
+</div>
+<?php include 'footer.php';?>
 </body>
 </html>
