bdee6db98c8a06fff6524b131dedd246cd6f186e
Andrew <atl9gz@virginia.edu>
2015-06-22 00:21:15 -0400

    JS back working

--- js/dependencies.php
+++ js/dependencies.php
@@ -1,21 +1,21 @@
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="../css/bootstrap.min.css">
 
 <!-- Optional theme -->
-<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="../css/bootstrap-theme.min.css">
 
 <!-- jQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="js/jquery.bootstrap-growl.js"></script>
+<script src="../js/jquery.bootstrap-growl.js"></script>
 
 <!-- Javascript -->
-<script src="js/bootstrap.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
 
 <!-- Custom Fonts -->
 <link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
 
 <!-- Custom CSS -->
-<link href="css/styles.css" rel="stylesheet">
+<link src="../css/styles.css" rel="stylesheet">
 
 <!-- Title and Favicon -->
 <title>Surety Security</title>
