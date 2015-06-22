3a3d1df7d0b1d542be34d80eaebc2f61b6b70228
Andrew <atl9gz@virginia.edu>
2015-06-22 00:44:35 -0400

    Everything working

--- js/dependencies.php
+++ js/dependencies.php
@@ -6,10 +6,10 @@
 
 <!-- jQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="/jquery.bootstrap-growl.js"></script>
+<script src="jquery.bootstrap-growl.js"></script>
 
 <!-- Javascript -->
-<script src="/bootstrap.min.js"></script>
+<script src="bootstrap.min.js"></script>
 
 <!-- Custom Fonts -->
 <link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
