47c6f3382a8521765c6a59d3dfa6d0cdf61ac0bf
Andrew <atl9gz@virginia.edu>
2015-06-22 00:34:51 -0400

    Javascript is in js folder

--- js/dependencies.php
+++ js/dependencies.php
@@ -1,22 +1,22 @@
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" src="../css/bootstrap.min.css">
+<link rel="stylesheet" href="../css/bootstrap.min.css">
 
 <!-- Optional theme -->
-<link rel="stylesheet" src="../css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="../css/bootstrap-theme.min.css">
 
 <!-- jQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="../js/jquery.bootstrap-growl.js"></script>
+<script src="jquery.bootstrap-growl.js"></script>
 
 <!-- Javascript -->
-<script src="../js/bootstrap.min.js"></script>
+<script src="bootstrap.min.js"></script>
 
 <!-- Custom Fonts -->
 <link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
 
 <!-- Custom CSS -->
-<link src="../css/styles.css" rel="stylesheet">
+<link href="../css/styles.css" rel="stylesheet">
 
 <!-- Title and Favicon -->
 <title>Surety Security</title>
-<link rel="shortcut icon" src="../img/logo.ico">
+<link rel="shortcut icon" href="../img/logo.ico">
