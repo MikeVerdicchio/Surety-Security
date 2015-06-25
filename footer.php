5db419ea78f9b29c9b1d43777e69afb219f167af
Mike Verdicchio <mikeverdicchio@gmail.com>
2015-06-25 11:24:28 -0400

    Updated footer with CC, no copyright

--- footer.php
+++ footer.php
@@ -2,11 +2,10 @@
     <hr>
     <div class="row">
         <div class="col-lg-12">
-            <p class="text-muted text-center">Copyright &copy; Surety Security 2015. All rights reserved.
+            <p class="text-muted text-center">
                 <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
-                    <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" />
+                    <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" />
                 </a>
-                <br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.</p>
-        </div>
+                <br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons BY-NC-SA 4.0 License</a>.</div>
     </div>
 </footer>
