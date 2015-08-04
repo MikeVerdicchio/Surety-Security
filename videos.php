577a5ef058cab3e2681c2d66cade6b110e0615be
Andrew <atl9gz@virginia.edu>
2015-08-04 15:01:49 -0400

    2 factor video

--- videos.php
+++ videos.php
@@ -10,7 +10,7 @@
 
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Videos <small>End Users</small></h1>
+                <h1 class="page-header">Videos</h1>
             </div>
         </div>
 
@@ -27,17 +27,15 @@
 
         <hr>
         <!-- Video 2: What is Two-Factor Authentication? -->
-        <!--
         <div class="row">
             <div class="col-md-7">
-                <iframe width="600" height="300" src="https://www.youtube.com/embed/Wv0EgRVKaXw?hd1080" frameborder="0" allowfullscreen></iframe>
+                <iframe width="600" height="300" src="https://www.youtube.com/embed/Da4lp54v6ng?hd1080" frameborder="0" allowfullscreen></iframe>
             </div>
             <div class="col-md-5">
                 <h3>What is Two-Factor Authentication?</h3>
                 <p>Two Factor Authentication takes your Internet security to a whole new level by requiring two "keys" to get into your accounts. One key is something you <b>know</b> like your typical username/password combination. The second key is something you <b>have</b> such as a fingerprint or a randomly-generated code from an app on your smartphone.</p>
             </div>
         </div>
--->
         <?php include 'footer.php';?>
     </div>
 
