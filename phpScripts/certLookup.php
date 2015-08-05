548478a2976fb2aae1965562611eb298634a0245
Andrew <atl9gz@virginia.edu>
2015-08-05 00:03:29 -0400

    Protocol showing

--- phpScripts/certLookup.php
+++ phpScripts/certLookup.php
@@ -3,6 +3,8 @@ $site= htmlspecialchars($_REQUEST["site"]);
 exec('openssl s_client -connect '. $site.':443 < /dev/null',$output);
 echo "<br>";
 foreach($output as $line){
+	if(strpos($line,'Protocol') !== false){
 	echo $line . "<br>";
+}
 }
 ?>
