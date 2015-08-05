17255c888b5d9293cf38fcee557b1b069af23995
Andrew <atl9gz@virginia.edu>
2015-08-05 19:32:47 -0400

    Cert chain update

--- phpScripts/certLookup.php
+++ phpScripts/certLookup.php
@@ -3,9 +3,14 @@ $site= htmlspecialchars($_REQUEST["site"]);
 exec('openssl s_client -connect '. $site.':443 < /dev/null',$output);
 echo "<br>";
 $certChain = array();
+$currentCert = "";
 foreach($output as $line){
 	if(strpos($line,'CN=') !== false){
-		array_push($certChain, 4);
+		$cert = explode('CN=', $line);
+		if($cert[1] != $currentCert){
+		array_push($certChain, $cert[1]);
+		$currentCert = $cert[1];
+	}
 	}
 	if(strpos($line,'END CERTIFICATE') !== false){
 		print_r($certChain);
@@ -17,7 +22,7 @@ foreach($output as $line){
 	}
 	if(strpos($line,'Protocol') !== false){
 		$protocol = explode(":", $line);
-		echo  "Version of SSL/TLS: <b>". $protocol[1] . "</b><br>";
+		echo  "Version of SSL/TLS: ". $protocol[1] . "<br>";
 	}
 }
 ?>
